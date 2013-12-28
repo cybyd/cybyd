9a8f4b43e4e0603dcef882d5877c23ca9b3b1670
cybernet <cybernet2u@users.sourceforge.net>
2013-12-28 17:53:19 +0200

    small changes ( still not working )

--- api/api.php
+++ api/api.php
@@ -4,9 +4,9 @@
 		This is an example class script proceeding secured API
 		To use this class you should keep same as query string and function name
 		Ex: If the query string value rquest=delete_user Access modifiers doesn't matter but function should be
-		     function delete_user(){
+		     function delete_user() {
 				 You code goes here
-			 }
+			 		    }
 		Class will execute the function dynamically;
 		
 		usage :
@@ -24,14 +24,16 @@
 		I used the below table for demo purpose.
 		
 		CREATE TABLE IF NOT EXISTS `users` (
-		  `user_id` int(11) NOT NULL AUTO_INCREMENT,
+		  `id` int(11) NOT NULL AUTO_INCREMENT,
 		  `user_fullname` varchar(25) NOT NULL,
-		  `user_email` varchar(50) NOT NULL,
-		  `user_password` varchar(50) NOT NULL,
-		  `user_status` tinyint(1) NOT NULL DEFAULT '0',
-		  PRIMARY KEY (`user_id`)
-		) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+		  `email` varchar(50) NOT NULL,
+		  `password` varchar(50) NOT NULL,
+		  `api_enabled` tinyint(1) NOT NULL DEFAULT '0'
+		  PRIMARY KEY (`id`)
+		) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
  	*/
+
+// THiS iS NOT A WORKiNG APi
 	
 	require_once("inc/rest.inc.php");
 	
@@ -54,9 +56,9 @@
 		/*
 		 *  Database connection 
 		*/
-		private function dbConnect(){
+		private function dbConnect() {
 			$this->db = mysql_connect(self::DB_SERVER,self::DB_USER,self::DB_PASSWORD);
-			if($this->db)
+			if ($this->db)
 				mysql_select_db(self::DB,$this->db);
 		}
 		
@@ -65,9 +67,9 @@
 		 * This method dynmically call the method based on the query string
 		 *
 		 */
-		public function processApi(){
+		public function processApi() {
 			$func = strtolower(trim(str_replace("/","",$_REQUEST['rquest'])));
-			if((int)method_exists($this,$func) > 0)
+			if ( (int)method_exists($this,$func) > 0)
 				$this->$func();
 			else
 				$this->response('',404);				// If the method not exist with in this class, response would be "Page not found".
@@ -90,10 +92,10 @@
 			$password = $this->_request['pwd'];
 			
 			// Input validations
-			if(!empty($email) and !empty($password)){
-				if(filter_var($email, FILTER_VALIDATE_EMAIL)){
-					$sql = mysql_query("SELECT id, username, email FROM xList_ro_users WHERE email = '$email' AND user_password = '".md5($password)."' LIMIT 1", $this->db);
-					if(mysql_num_rows($sql) > 0){
+			if (!empty($email) and !empty ($password) ) {
+				if (filter_var ($email, FILTER_VALIDATE_EMAIL) ) {
+					$sql = mysql_query("SELECT id, username, email FROM xList_ro_users WHERE email = '$email' AND password = '".md5($password)."' LIMIT 1", $this->db);
+					if (mysql_num_rows($sql) > 0) {
 						$result = mysql_fetch_array($sql,MYSQL_ASSOC);
 						
 						// If success everythig is good send header as "OK" and user details
@@ -108,15 +110,15 @@
 			$this->response($this->json($error), 400);
 		}
 		
-		private function users(){	
+		private function users() {	
 			// Cross validation if the request method is GET else it will return "Not Acceptable" status
-			if($this->get_request_method() != "GET"){
+			if ($this->get_request_method() != "GET") {
 				$this->response('',406);
 			}
 			$sql = mysql_query("SELECT id, username, email FROM xList_ro_users WHERE user_status = 1", $this->db);
-			if(mysql_num_rows($sql) > 0){
+			if (mysql_num_rows($sql) > 0) {
 				$result = array();
-				while($rlt = mysql_fetch_array($sql,MYSQL_ASSOC)){
+				while ($rlt = mysql_fetch_array($sql,MYSQL_ASSOC) ) {
 					$result[] = $rlt;
 				}
 				// If success everythig is good send header as "OK" and return list of users in JSON format
@@ -125,25 +127,25 @@
 			$this->response('',204);	// If no records "No Content" status
 		}
 		
-		private function deleteUser(){
+		private function deleteUser() {
 			// Cross validation if the request method is DELETE else it will return "Not Acceptable" status
-			if($this->get_request_method() != "DELETE"){
+			if ($this->get_request_method() != "DELETE") {
 				$this->response('',406);
 			}
 			$id = (int)$this->_request['id'];
-			if($id > 0){				
-				mysql_query("DELETE FROM xList_ro_users WHERE user_id = $id LIMIT 1");
+			if ($id > 0) {				
+				mysql_query("DELETE FROM xList_ro_users WHERE id = $id LIMIT 1");
 				$success = array('status' => "Success", "msg" => "Successfully one record deleted.");
 				$this->response($this->json($success),200);
-			}else
+			} else
 				$this->response('',204);	// If no records "No Content" status
 		}
 		
 		/*
 		 *	Encode array into JSON
 		*/
-		private function json($data){
-			if(is_array($data)){
+		private function json($data) {
+			if (is_array($data) ) {
 				return json_encode($data);
 			}
 		}
