9a199a4cad32f41c975be3ba24096eec394e4246
cybernet <cybernet2u@users.sourceforge.net>
2013-12-28 19:07:36 +0200

    spacing

--- api/inc/rest.inc.php
+++ api/inc/rest.inc.php
@@ -11,22 +11,22 @@
 		private $_method = "";		
 		private $_code = 200;
 		
-		public function __construct(){
+		public function __construct() {
 			$this->inputs();
 		}
 		
-		public function get_referer(){
+		public function get_referer() {
 			return $_SERVER['HTTP_REFERER'];
 		}
 		
-		public function response($data,$status){
+		public function response($data,$status) {
 			$this->_code = ($status)?$status:200;
 			$this->set_headers();
 			echo $data;
 			exit;
 		}
 		
-		private function get_status_message(){
+		private function get_status_message() {
 			$status = array(
 						100 => 'Continue',  
 						101 => 'Switching Protocols',  
@@ -69,15 +69,15 @@
 						503 => 'Service Unavailable',  
 						504 => 'Gateway Timeout',  
 						505 => 'HTTP Version Not Supported');
-			return ($status[$this->_code])?$status[$this->_code]:$status[500];
+			return ($status[$this->_code]) ? $status[$this->_code]:$status[500];
 		}
 		
-		public function get_request_method(){
+		public function get_request_method() {
 			return $_SERVER['REQUEST_METHOD'];
 		}
 		
-		private function inputs(){
-			switch($this->get_request_method()){
+		private function inputs() {
+			switch($this->get_request_method() ) {
 				case "POST":
 					$this->_request = $this->cleanInputs($_POST);
 					break;
@@ -95,15 +95,15 @@
 			}
 		}		
 		
-		private function cleanInputs($data){
+		private function cleanInputs($data) {
 			$clean_input = array();
-			if(is_array($data)){
-				foreach($data as $k => $v){
+			if (is_array($data) ) {
+				foreach($data as $k => $v) {
 					$clean_input[$k] = $this->cleanInputs($v);
 				}
 			}else{
-				if(get_magic_quotes_gpc()){
-					$data = trim(stripslashes($data));
+				if (get_magic_quotes_gpc() ) {
+					$data = trim (stripslashes($data) );
 				}
 				$data = strip_tags($data);
 				$clean_input = trim($data);
@@ -111,8 +111,8 @@
 			return $clean_input;
 		}		
 		
-		private function set_headers(){
-			header("HTTP/1.1 ".$this->_code." ".$this->get_status_message());
+		private function set_headers() {
+			header("HTTP/1.1 ".$this->_code." ".$this->get_status_message() );
 			header("Content-Type:".$this->_content_type);
 		}
 	}	
