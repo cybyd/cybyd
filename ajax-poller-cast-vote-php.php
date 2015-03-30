4bcdd46f67d53eadb6498af62168535710a53c3d
cybernet <cybernet2u@users.sourceforge.net>
2015-03-30 14:00:10 +0300

    The road to #MySQLi

--- ajax-poller-cast-vote-php.php
+++ ajax-poller-cast-vote-php.php
@@ -1,10 +1,10 @@
 <?php
 
-// CyBerFuN.ro & xList.ro
+// xDNS.ro & xLiST.ro
 
-// xList .::. xDNS
+// xLiST .::. xDNS
 // http://xDNS.ro/
-// http://xLIST.ro/
+// http://xLiST.ro/
 // Modified By cybernet2u
 
 /////////////////////////////////////////////////////////////////////////////////////
@@ -59,24 +59,24 @@ if(isset($_GET['pollId'])){
   // Insert new vote into the database
   // You may put in some more code here to limit the number of votes the same ip adress could cast.
 
-  if($optionId)mysql_query("INSERT INTO {$TABLE_PREFIX }poller_vote(pollerID,optionID,ipAddress,voteDate,memberID) VALUES('".$pollId."','".$optionId."','".ip2long(getenv("REMOTE_ADDR"))."',unix_timestamp(),'".$userID."')");
+  if($optionId)mysqli_query($GLOBALS["___mysqli_ston"], "INSERT INTO {$TABLE_PREFIX }poller_vote(pollerID,optionID,ipAddress,voteDate,memberID) VALUES('".$pollId."','".$optionId."','".ip2long(getenv("REMOTE_ADDR"))."',unix_timestamp(),'".$userID."')");
 
   // Returning data as xml
 
   echo '<?xml version="1.0" ?>';
 
-  $res = mysql_query("select ID,pollerTitle from {$TABLE_PREFIX}poller where ID='".$pollId."'");
-  if($inf = mysql_fetch_array($res)){
+  $res = mysqli_query($GLOBALS["___mysqli_ston"], "select ID,pollerTitle from {$TABLE_PREFIX}poller where ID='".$pollId."'");
+  if($inf = mysqli_fetch_array($res)){
     echo "<pollerTitle>".unesc($inf["pollerTitle"])."</pollerTitle>\n";
 
-    $resOptions = mysql_query("select ID,optionText from {$TABLE_PREFIX}poller_option where pollerID='".$inf["ID"]."' order by pollerOrder") or die(mysql_error());
-    while($infOptions = mysql_fetch_array($resOptions)){
+    $resOptions = mysqli_query($GLOBALS["___mysqli_ston"], "select ID,optionText from {$TABLE_PREFIX}poller_option where pollerID='".$inf["ID"]."' order by pollerOrder") or die(((is_object($GLOBALS["___mysqli_ston"])) ? mysqli_error($GLOBALS["___mysqli_ston"]) : (($___mysqli_res = mysqli_connect_error()) ? $___mysqli_res : false)));
+    while($infOptions = mysqli_fetch_array($resOptions)){
       echo "<option>\n";
       echo "\t<optionText>".unesc($infOptions["optionText"])."</optionText>\n";
       echo "\t<optionId>".$infOptions["ID"]."</optionId>\n";
 
-      $resVotes = mysql_query("select count(ID) from {$TABLE_PREFIX}poller_vote where optionID='".$infOptions["ID"]."' AND pollerID='".$inf["ID"]."'");
-      if($infVotes = mysql_fetch_array($resVotes)){
+      $resVotes = mysqli_query($GLOBALS["___mysqli_ston"], "select count(ID) from {$TABLE_PREFIX}poller_vote where optionID='".$infOptions["ID"]."' AND pollerID='".$inf["ID"]."'");
+      if($infVotes = mysqli_fetch_array($resVotes)){
         echo "\t<votes>".$infVotes["count(ID)"]."</votes>\n";
       }
       echo "</option>";
